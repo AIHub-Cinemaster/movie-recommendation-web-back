566d8c59d1616f9cd9f2eeaee4a89ef98e73eebf
jisulee42 <leejisu1584@gmail.com>
2022-08-04 14:24:54 +0900

    test

--- routes/starRating.js
+++ routes/starRating.js
@@ -5,7 +5,7 @@ const asyncHandler = require("./../utils/async-handler");
 
 const router = Router();
 
-// 별점 가져오기
+// 별점 가져오기 2
 router.get(
   "/:movieId",
   asyncHandler(async (req, res, next) => {
