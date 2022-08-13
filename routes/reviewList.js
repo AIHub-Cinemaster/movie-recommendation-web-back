7bb7be5283ebe239386b1e9279d7b1c8121f718b
Hyyena <sjtxm0320@gmail.com>
2022-08-14 02:05:24 +0900

    🎨 Add star rating to review list

--- routes/reviewList.js
+++ routes/reviewList.js
@@ -10,13 +10,21 @@ const router = Router();
 /*
 * Read.
 리뷰 목록 조회
+
+TODO : 평점 추가
+? populate 조건식 추가
+
+TODO : 댓글 추가
+TODO : 좋아요 추가
 */
 router.get(
   "/:movieId",
   asyncHandler(async (req, res) => {
     const { movieId } = req.params;
 
-    const reviews = await Review.find({ movieId: movieId }).populate("userRef");
+    const reviews = await Review.find({ movieId: movieId })
+      .populate("userRef")
+      .populate("starRef", { starList: { $elemMatch: { movieId: movieId } } });
 
     if (reviews.length === 0) {
       res.status(404);
@@ -31,12 +39,9 @@ router.get(
           author: review.userRef.name,
           title: review.title,
           content: review.content,
-          createdAt: moment(review.createdAt).format("YYYY년 M월 D일"),
-          updatedAt: moment(review.updatedAt).format("YYYY년 M월 D일"),
-
-          // TODO : 평점 추가
-          // TODO : 댓글 추가
-          // TODO : 좋아요 추가
+          star: review.starRef.starList[0].star,
+          createdAt: moment(review.createdAt).fromNow(),
+          updatedAt: moment(review.updatedAt).fromNow(),
         };
         return data;
       }),
