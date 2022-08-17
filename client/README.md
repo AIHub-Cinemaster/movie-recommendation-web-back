ce8c5a58b1f0cd73557436ec8fb63c29f282307a
이지수 <leejisu1584@gmail.com>
2022-08-17 14:30:31 +0900

    Delete client directory

--- client/README.md
+++
@@ -1,4 +0,0 @@
-# movie-recommendation-web
-
-추천시스템
-AI Hub 양재 인공지능 웹 개발 프론트엔드
--- client/src/config.js
+++
@@ -1,6 +0,0 @@
-//config.js
-import axios from "axios";
-
-export const axiosInstance = axios.create({
-  baseURL: "https://iwipwq-nest.herokuapp.com/api/",
-});
