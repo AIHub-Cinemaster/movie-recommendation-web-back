f99d4df8d2af2ffaf63f5c6682330d5552a65dcb
Hyyena <sjtxm0320@gmail.com>
2022-08-18 19:10:49 +0900

    🎨 Add response "likeUsers" to the review API

--- routes/reviewList.js
+++ routes/reviewList.js
@@ -11,6 +11,8 @@ const router = Router();
 * Read.
 영화별 리뷰 목록 조회
 
+! FIXME: 리뷰 작성 후 조회 시 간헐적으로 500 Error 발생
+
 TODO: 리뷰 정렬 (좋아요 > 리뷰/별점 > 별점)
 TODO: 추천 알고리즘 수정
 
@@ -27,11 +29,36 @@ router.get(
       .populate("starRef", { starList: { $elemMatch: { movieId: movieId } } })
       .populate("likeRef");
 
-    console.log(reviews);
-
     const result = await Promise.all(
       reviews.map((review) => {
-        console.log(review);
+        let like = review.likeRef.likeUsers.find((element) => {
+          if (element.user === review.userRef.shortId) {
+            return true;
+          }
+        });
+
+        /*
+         * 프론트 요청
+         * 영화별 리뷰 조회 시 유저의 좋아요 유/무 판별을 위해
+         * 각 리뷰 데이터에 좋아요 누른 유저를 배열로 추가
+         */
+        const likeUsers = review.likeRef.likeUsers;
+        let userList = [];
+
+        likeUsers.map((user) => {
+          userList.push(user.user);
+        });
+
+        /*
+         * 총 좋아요 수
+         */
+
+        if (!like) {
+          like = false;
+        } else {
+          like = like.like;
+        }
+
         let likeCount = review.likeRef.likeCount;
 
         if (likeCount >= 1) {
@@ -51,6 +78,7 @@ router.get(
           star: review.starRef.starList[0].star,
           createdAt: moment(review.createdAt).fromNow(),
           updatedAt: moment(review.updatedAt).fromNow(),
+          likeUsers: userList,
           likeCount: likeCount,
         };
         return data;
